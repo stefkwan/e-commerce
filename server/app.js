6598085700bcc6f9cfff6a75f287374daf48e810
stefkwan <stefanikwan@gmail.com>
2019-07-02 21:39:23 +0700

    added port for deploy

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const routes = require('./routes')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var cors = require('cors')
 app.use(cors())
